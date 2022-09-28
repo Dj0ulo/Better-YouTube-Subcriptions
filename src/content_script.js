ff573d78d79bcfc89b4f825c7e09e2d938efc0d5
Julien Launois <46035353+Dj0ulo@users.noreply.github.com>
2022-09-28 18:30:35 +0200

    Watched videos

--- src/content_script.js
+++ src/content_script.js
@@ -1,5 +1,8 @@
 (async function () {
   console.debug("Better Youtube Subscriptions: Content Script Loaded");
+  const startytInitialData = 'var ytInitialData = ';
+  let ytInitialData = [...document.body.querySelectorAll('script')].find(e => e.innerHTML.trim().startsWith(startytInitialData))?.innerHTML.trim();
+  ytInitialData = JSON.parse(ytInitialData.slice(startytInitialData.length, -1));
 
   const docHead = document.head || document.documentElement;
   const styles = ['style'];
@@ -8,15 +11,19 @@
 
   // GLOBAL VARIABLES
   let observerNewVideos = null;
-  let toggleShorts = false;
+  let showShorts = false;
+  let showWatched = false;
   let switchButton = document.getElementById(ID_SWITCH_BUTTON);
+  let showWatchedButton = document.getElementById(ID_SHOW_WATCHED);
   let prevIsGrid = isGrid();
+  let nProgressBars = 0;
+
 
   async function execute() {
     if (!isSubcriptionsPage(document.location.href)) {
       return
     }
-    console.log("Running on subscriptions page");
+    console.debug("Executing on subscriptions page");
 
     const contents = await new Promise(resolve => {
       const getContents = () => document.querySelector("ytd-browse ytd-section-list-renderer > #contents");
@@ -33,7 +40,7 @@
       observer.observe(document.body, { childList: true, subtree: true });
     });
 
-    // FUNCTIONS THAT DEPENDS ON THE contents VARIABLE
+    // FUNCTIONS THAT DEPEND ON THE contents VARIABLE
     const querySections = () => [...contents.querySelectorAll("ytd-item-section-renderer")];
     const menuButtons = () => {
       const manageSubscriptions = [...contents.querySelectorAll('ytd-item-section-renderer #title-container #subscribe-button')]
@@ -47,46 +54,66 @@
         const titleContainer = firstVisibleSection.querySelector('#title-container');
         menuButtons().forEach(button => titleContainer.appendChild(button));
         titleContainer.appendChild(switchButton);
+        titleContainer.appendChild(showWatchedButton);
       }
     }
 
     const hideFrom = (elements) => {
-      elements.forEach(e => {
-        if ((isShort(e) === undefined) || (isShort(e) && toggleShorts) || (!isShort(e) && !toggleShorts))
-          return;
-        // console.log(toggleShorts, 'hiding', e.querySelector('ytd-thumbnail a#thumbnail').href);
-        if (!isGrid()) {
-          e.parentNode.parentNode.parentNode.parentNode.parentNode.setAttribute(ATTR_HIDDEN, '');
-          const menu = e.parentNode.parentNode.parentNode.querySelector('#title-container #menu');
-          if (menu && menu.innerHTML.trim() !== "") {
-            menuToTop();
+      elements
+        .filter(e => {
+          const short = isShort(e);
+          const watched = isWatched(e);
+          if (short === undefined)
+            return false;
+          return /*(showShorts && !short) ||*/ (!showShorts && short) || (!showWatched && !short && watched)
+        })
+        .forEach(e => {
+          if (!isGrid()) {
+            e.parentNode.parentNode.parentNode.parentNode.parentNode.setAttribute(ATTR_HIDDEN, '');
+            const menu = e.parentNode.parentNode.parentNode.querySelector('#title-container #menu');
+            if (menu && menu.innerHTML.trim() !== "") {
+              menuToTop();
+            }
+          } else {
+            e.setAttribute(ATTR_HIDDEN, "");
           }
-        } else {
-          e.setAttribute(ATTR_HIDDEN, "");
-        }
-      });
+        });
     }
     const setup = (firstTime = true) => {
       prevIsGrid = isGrid();
-      contents.setAttribute("display-video-kind", toggleShorts ? "shorts" : "videos");
+      contents.setAttribute("display-video-kind", showShorts ? "shorts" : "videos");
+      contents.setAttribute("display-watched", showWatched ? "true" : "false");
       if (!firstTime)
         showAll();
       menuToTop();
       hideFrom(queryVideos(contents));
+      nProgressBars = 0;
     }
 
     if (!switchButton) {
       switchButton = el("button", { id: ID_SWITCH_BUTTON, className: "style-scope ytd-toggle-button-renderer style-text" });
       el("div", { className: "shorts-icon style-scope", innerHTML: ICON_SHORTS }, switchButton);
       switchButton.onclick = () => {
-        toggleShorts = !toggleShorts;
+        showShorts = !showShorts;
         setup(false);
       };
     }
-
+    if (!showWatchedButton) {
+      showWatchedButton = el("button", { id: ID_SHOW_WATCHED, className: "style-scope ytd-toggle-button-renderer style-text" });
+      el("div", { className: "eye-icon style-scope", innerHTML: ICON_EYE }, showWatchedButton);
+      showWatchedButton.onclick = () => {
+        showWatched = !showWatched;
+        setup(false);
+      };
+    }
     observerNewVideos = new MutationObserver((mutationRecords) => {
       if (isGrid() !== prevIsGrid) {
         setup(false);
+      }
+      const progressBars = contents.querySelectorAll('#progress');
+      if (nProgressBars != progressBars.length) {
+        hideFrom(queryVideos(contents));
+        nProgressBars = progressBars.length;
       }
       const itemSections = [...mutationRecords[0].addedNodes].filter(n => n.matches("ytd-item-section-renderer"));
       itemSections.forEach(section => hideFrom(queryVideos(section)));
--- src/utils.js
+++ src/utils.js
@@ -29,7 +29,10 @@ function el(tag, attr, parent) {
 // CONSTANTS
 const ATTR_HIDDEN = "better-hide-this-content";
 const ICON_SHORTS = `<svg viewBox="0 0 16 16" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g width="16" height="16" viewBox="0 0 16 16" class="style-scope yt-icon"><path d="M10.65,1C10.65,1,10.65,1,10.65,1c-0.37,0-0.75,0.1-1.09,0.31L4.25,4.46C3.44,4.93,2.96,5.89,3,6.9  C3.05,7.9,3.58,8.77,4.39,9.18c0.02,0.01,0.75,0.35,0.75,0.35l-0.9,0.53c-1.14,0.68-1.58,2.27-0.98,3.55C3.69,14.49,4.5,15,5.35,15  c0.37,0,0.74-0.1,1.09-0.31l5.31-3.15c0.8-0.48,1.29-1.43,1.24-2.45c-0.04-0.99-0.58-1.87-1.39-2.27c-0.02-0.01-0.75-0.35-0.75-0.35  l0.9-0.53c1.14-0.68,1.58-2.27,0.97-3.55C12.31,1.51,11.49,1,10.65,1L10.65,1z" class="style-scope yt-icon"></path></g></svg>`;
+const ICON_EYE = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>eye</title><path d="M10 7.5a2.5 2.5 0 1 0 2.5 2.5A2.5 2.5 0 0 0 10 7.5zm0 7a4.5 4.5 0 1 1 4.5-4.5 4.5 4.5 0 0 1-4.5 4.5zM10 3C3 3 0 10 0 10s3 7 10 7 10-7 10-7-3-7-10-7z"/></svg>`;
 const ID_SWITCH_BUTTON = "better-youtube-subscriptions-switch-shorts";
+const ID_SHOW_WATCHED = "better-youtube-subscriptions-show-watched";
+const MIN_WATCHED = 0.1;
 
 const isGrid = () => !!document.querySelector("ytd-grid-renderer");
 const showAll = () => {
@@ -42,4 +45,10 @@ const isShort = (video) => {
   if (!thumbnailLink)
     return undefined;
   return thumbnailLink.href.includes('/shorts/');
+};
+const isWatched = (video) => {
+  const progressBar = video.querySelector('#progress');
+  if (!progressBar)
+    return undefined;
+  return parseInt(progressBar.style.width) / 100 >= MIN_WATCHED;
 };
